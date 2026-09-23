555302bcd76610b99b2b5a40d77baba93d59c98d
nroede <nilsroedeno@gmail.com>
2026-09-23 18:15:34 -0400

    checkin2 second ver

--- labs/checkin2/gripper.py
+++ labs/checkin2/gripper.py
@@ -22,12 +22,30 @@ neutral_q = arm.neutral_position()
 arm.safe_move_to_position(neutral_q)
 
 print(arm.get_gripper_state())
-arm.open_gripper()
-print(arm.get_gripper_state())
-rospy.sleep(5)
 arm.close_gripper()
 print(arm.get_gripper_state())
+rospy.sleep(5)
+arm.open_gripper()
+print(arm.get_gripper_state())
+
+lowered_q = np.array([0,-1,0,-2,0,1,1]) # TODO: change this configuration to lower the arm to a position where the gripper is near the table
+arm.safe_move_to_position(lowered_q)
+rospy.sleep(3)
 
 ### ADD STUDENT CODE HERE ###
 
 #arm.exec_gripper_cmd(0.1)
+
+### END STUDENT CODE ###
+
+rospy.sleep(3)
+raised_q = np.array([0,-1,0,-2,0,1,1]) # TODO: change this configuration to raise the arm to a position where the gripper is away from the table
+arm.safe_move_to_position(raised_q)
+
+rospy.sleep(5)
+
+arm.safe_move_to_position(lowered_q)
+arm.open_gripper()
+
+rospy.sleep(5)
+arm.safe_move_to_position(neutral_q)
